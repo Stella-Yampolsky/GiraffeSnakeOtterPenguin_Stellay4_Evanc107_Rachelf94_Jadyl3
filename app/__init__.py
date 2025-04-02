16716068810ad119c8ec17a38ca61aeba3cbe4dc
evan c <evanc@macbookpro.myfiosgateway.com>
2025-04-02 00:23:47 -0400

    flask running

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, flash
+from flask import Flask, render_template, request, redirect, flash, url_for
 import sqlite3
 
 app = Flask(__name__)
@@ -6,7 +6,7 @@ app.config['SECRET_KEY'] = 'your_secret_key'
 
 @app.route('/')
 def landing():
-    return
+    return redirect(url_for('home'))
 
 @app.route('/home')
 def home():
